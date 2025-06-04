dfc2c7b86958a5ceb7b40bea6148fd18cd217686
Naimur Rahman Naim <naimekattor@gmail.com>
2025-06-05 03:11:34 +0600

    added title

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the User Management App</h1>
+    <div className="flex h-screen justify-center items-center">
+      <h1 className=" font-semibold text-[22px] text-[#002058]">
+        Welcome to the User Management App
+      </h1>
     </div>
   );
 }
